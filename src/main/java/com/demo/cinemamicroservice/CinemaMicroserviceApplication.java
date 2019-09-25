8fbf3f6a3aa9b3c273fa48ed761d18ac0c11cb7f
edward ntim <edward.ntim@capgemini.com>
2019-09-25 11:47:31 +0100

    Spring Setup

---
+++ src/main/java/com/demo/cinemamicroservice/CinemaMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.demo.cinemamicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaMicroserviceApplication.class, args);
+	}
+
+}
