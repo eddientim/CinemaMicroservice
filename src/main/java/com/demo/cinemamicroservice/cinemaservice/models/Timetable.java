27ffd97b95278131e1ec2c720b494df71cba49e3
edward ntim <edward.ntim@capgemini.com>
2019-12-06 21:01:59 +0000

    WIP need to fix Timetable class

--- src/main/java/com/demo/cinemamicroservice/cinemaservice/models/Timetable.java
+++ src/main/java/com/demo/cinemamicroservice/cinemaservice/models/Timetable.java
@@ -1,5 +1,55 @@
 package com.demo.cinemamicroservice.cinemaservice.models;
 
-public class Timetable {
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
+import java.util.Collection;
+
+@DynamoDBTable(tableName = "timetable")
+public class Timetable extends BaseModel implements Comparable<Timetable> {
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
+    private Date startDateTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
+    private Date endDateTime;
+    private Collection<CinemaShowing> cinemaShowings;
+
+    public Timetable(Date startDateTime, Date endDateTime) {
+        this.startDateTime = startDateTime;
+        this.endDateTime = endDateTime;
+    }
+
+    @DynamoDBAttribute
+    public Date getStartDateTime() {
+        return startDateTime;
+    }
+
+    public void setStartDateTime(Date startDateTime) {
+        this.startDateTime = startDateTime;
+    }
+    @DynamoDBAttribute
+    public Date getEndDateTime() {
+        return endDateTime;
+    }
+
+    public void setEndDateTime(Date endDateTime) {
+        this.endDateTime = endDateTime;
+    }
+
+    @DynamoDBAttribute
+    public Collection<CinemaShowing> getCinemaShowings() {
+        return cinemaShowings;
+    }
+
+    public void setCinemaShowings(Collection<CinemaShowing> cinemaShowings) {
+        this.cinemaShowings = cinemaShowings;
+    }
+
+    @Override
+    public int compareTo(Timetable t) {
+        if (startDateTime.equals(t.startDateTime)) return 0;
+        else if (startDateTime.before(t.startDateTime)) return 1;
+        else return -1;
+    }
 }
